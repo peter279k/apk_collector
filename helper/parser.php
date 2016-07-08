0b5477b7ad8388cae2045186352c40f62b3b17d6
peter279k <peter279k@gmail.com>
2016-07-08 23:08:13 +0800

    add apkmirror/ ignore (apk files) mofdify crawler

--- helper/parser.php
+++ helper/parser.php
@@ -4,10 +4,20 @@
 	use GuzzleHttp\Exception\RequestException;
 	use Symfony\Component\DomCrawler\Crawler;
 
-	//apkmirrors
-	function parse_apkmirror_html($html_contents) {
+	/*
+		apkmirror
+		@author: peter
+		@date: 2016/07/06
+		functions:
+			parse_apkmirror_html($urls, $html_contents)
+			get_apkmirror_pages($html_contents)
+			get_apkmirror_apk($url, $html_contents)
+			apkmirror_file_name($url)
+			download_apkmirror_file($url, $link)
+	*/
+	function parse_apkmirror_html($urls, $html_contents) {
 		$crawler = new Crawler($html_contents);
-		$link = $crawler->filter('a[class="fontBlack"]');
+		$link = $crawler -> filter('a[class="fontBlack"]');
 		$count = 0;
 		$links = array();
 
@@ -15,15 +25,17 @@
 			$crawler = new Crawler($value);
 			$url = $crawler -> filter('a') -> attr('href');
 			$res = in_array($url, $links);
-
-			if(!$res) {
+				
+			if(!$res && $res != "/uploads/") {
 				$links[$count] = $url;
 				$count += 1;	
 			}
 		}
 
 		foreach ($links as $value) {
-			file_put_contents("./helper/files/apkmirror.txt", $value . "\r\n", FILE_APPEND);
+			$client = new Client();
+            $response = $client -> get($urls . $value);
+			get_apkmirror_apk($urls, $response -> getBody() -> getContents());
 		}
 	}
 
@@ -37,22 +49,106 @@
 
 	function get_apkmirror_apk($url, $html_contents) {
 	 	$crawler = new Crawler($html_contents);
-	 	$download_direct = $crawler -> filter('a[data-google-vignette="false"]');
-	 	$link = $download_direct -> attr('href');
-
+		
+		//getting title whether the title is correct.
+		$title = $crawler -> filter('title') -> text();
+		
+		if($title == "Latest Uploads - APKMirror") {
+			//abort downloading apk file
+			return false;
+		}
+		
+		try {
+			$download_direct = $crawler -> filter('a[data-google-vignette="false"]');
+			$link = $download_direct -> attr('href');
+		}
+		catch(Exception $e) {
+			echo $e -> getMessage() . "\n";
+			file_put_contents("./helper/files/apkmirror/error_link_" . time() . ".txt", $html_contents, FILE_APPEND);
+			//abort downloading apk file.
+			return false;
+		}
+		
 	 	if ($link == "#downloads") {
-	 		//find correct download link
-
+	 		//finding correct download link
+			//e.g. /apk/facebook-2/facebook/facebook-85-0-0-5-70-release/facebook-85-0-0-5-70-android-apk-download/
+			$find_download = $crawler -> filter('div[class="table-cell rowheight addseparator expand pad dowrap"]');
+			
+			foreach($find_download as $key => $value) {
+				$crawler = new Crawler($value);
+				$link = $crawler -> filter('a') -> attr('href');
+				break;
+			}
+			
+			$client = new Client();
+            $response = $client -> get($url . $link);
+			get_apkmirror_apk($url, $response -> getBody() -> getContents());
 	 	}
-	 	else {
-	 		//download apk directly
-	 		$client = new Client();
-	 		$client->request('GET', $url . $link, [
-    				'sink' => './helper/files/apkmirror',
-			]);
-            		}
-
-            		return $link;
+		
+		else {
+			download_apkmirror_file($url, $link);
+		}
+	}
+	
+	//get the default apk file name
+	function apkmirror_file_name($url) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, TRUE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$response = curl_exec($ch);
+		$last_url = curl_getinfo($ch, CURLINFO_REDIRECT_URL);
+		curl_close($ch);
+		$arr = explode("/", $last_url);
+		$file_name = $arr[count($arr) - 1];
+		return $file_name;
+	}
+	
+	//downloading apk directly
+	function download_apkmirror_file($url, $link) {
+	
+		$file_name = apkmirror_file_name($url . $link);
+		$file_path = "./helper/files/apkmirror/" . $file_name;
+		
+		$is_exists = false;
+		
+		$handle = fopen("./helper/files/apkmirror/file_lists.txt", "r");
+		
+		while(!feof($handle)) {
+			$str = fgets($handle, 4096);
+			if($str == $file_name) {
+				$is_exists = true;
+				fclose($handle);
+				break;
+			}
+		}
+		
+		if(!file_exists($file_path) || !$is_exists) {
+			$client = new Client(['headers' => ['Keep-Alive' => '1000', 'Connection' => 'keep-alive']]);
+			try {
+				$response = $client -> request('GET', $url . $link, ["verify" => false, "sink" => $file_path, 'progress' => 
+					function ($dl_total_size, $dl_size_so_far, $ul_total_size, $ul_size_so_far) {
+						// do something.
+						if($dl_total_size !=0) {
+							echo "Progress: " . round(100 - (abs($dl_total_size - $dl_size_so_far - 100) / $dl_total_size * 100), 2) . "%\n";
+						}
+					}
+				]);
+			}
+			catch(Exception $e) {
+				file_put_contents("./helper/files/apkmirror/error_download_list.txt", $url . $link . "\r\n", FILE_APPEND);
+				echo "error download: " . $file_name . "\n";
+				echo "The Network error happened.\n";
+				$e -> getMessage() . "\n";
+			}
+			
+			echo "sleep 5 seconds...\n";
+			sleep(5);
+			echo "wake up !\n";
+		}
+		else {
+			echo "the apk file is existed.\n"
+		}
 	}
 
 ?>
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	use Psr\Http\Message\ResponseInterface;
 	use GuzzleHttp\Exception\RequestException;
 	use Symfony\Component\DomCrawler\Crawler;
-
+	
 	$page = 1;
 	$base_urls = array(
 		"http://www.apkmirror.com",
@@ -17,54 +17,27 @@
 		"http://www.androidapksfree.com/"
 	);
 
-	if(file_exists("helper/files/apkmirror.txt")) {
-		$handler = fopen("./helper/files/apkmirror.txt", "r");
-
-              	if($handler) {
-              		while(!feof($handler)) {
-              			$str = fgets($handler, 4096);
-              			$client = new Client();
-              			$promise = $client -> getAsync($base_urls[0] . $str);
-              			$promise->then(
-					function (ResponseInterface $res) {
-    						get_apkmirror_apk($res -> getBody() -> getContents());
-    					},
-    					function (RequestException $e) {
-        						echo $e->getMessage() . "\n";
-    					}
-				);
-                        		}
-
-                        		fclose($handler);
-                	}
-                	
-		exit();
-	}
-
 	$contents = array();
 	$pages = 0;
 
 	$client = new Client();
 	
-	$response = $client -> get($base_urls[0]);
+	try {
+		
+		$response = $client -> get($base_urls[0]);
 	
-	if($response -> getStatuscode() == 200)
-		$pages = get_apkmirror_pages($response -> getBody() -> getContents());
-	else
-		die("The code is not 200.");
-
+		if($response -> getStatuscode() == 200)
+			$pages = get_apkmirror_pages($response -> getBody() -> getContents());
+		else
+			die("The code is not 200.");
+	}
+	catch(Exception $e) {
+		die($e -> getMessage());
+	}
+	
 	for($page=1;$page<=$pages;$page++) {
-		$promise = $client -> getAsync($base_urls[0] . "/page/" . $page . "/");
-		$promise->then(
-			function (ResponseInterface $res) {
-    				parse_apkmirror_html($res -> getBody() -> getContents());
-    			},
-    			function (RequestException $e) {
-        				echo $e->getMessage() . "\n";
-    			}
-		);
-
-		$promise -> wait();
+		$response = $client -> get($base_urls[0] . "/page/" . $page . "/");
+		parse_apkmirror_html($base_urls[0], $response -> getBody() -> getContents());
 	}
 
 ?>
